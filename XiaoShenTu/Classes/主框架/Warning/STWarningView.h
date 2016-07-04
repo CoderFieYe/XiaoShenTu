48d92c591d384ce99a9f842fa4c1d00839f67709
万启鹏 <1067385702@qq.com>
2016-07-04 18:54:00 +0800

    最后提交

---
+++ XiaoShenTu/Classes/主框架/Warning/STWarningView.h
@@ -0,0 +1,13 @@
+//
+//  STWarningView.h
+//  XiaoShenTu
+//
+//  Created by 万启鹏 on 16/7/4.
+//  Copyright © 2016年 万启鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STWarningView : UIView
+
+@end
---
+++ XiaoShenTu/Classes/主框架/Warning/STWarningView.m
@@ -0,0 +1,21 @@
+//
+//  STWarningView.m
+//  XiaoShenTu
+//
+//  Created by 万启鹏 on 16/7/4.
+//  Copyright © 2016年 万启鹏. All rights reserved.
+//
+
+#import "STWarningView.h"
+
+@implementation STWarningView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
