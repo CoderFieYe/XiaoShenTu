748edb49cf843f4a7c66c7e52b90dd4f80983c8f
darren995286226 <995286226@qq.com>
2016-07-04 13:58:28 +0800

    6

--- XiaoShenTu/Classes/主框架/Warning/STWarningViewController.m
+++ XiaoShenTu/Classes/主框架/Warning/STWarningViewController.m
@@ -13,9 +13,7 @@
 #import "STHInfomationView.h"
 
 @interface STWarningViewController ()<MAMapViewDelegate>
-{
-    MAMapView *_mapView;
-}
+
 
 @end
 
@@ -23,26 +21,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-        self.view.backgroundColor = [UIColor whiteColor];
-    self.view.backgroundColor = [UIColor redColor];
-   
+
+     self.view.backgroundColor = [UIColor whiteColor];
     
 #pragma 地图展示
-//    //配置用户Key
-//    [MAMapServices sharedServices].apiKey = KGaoDeKey;
-//    
-//    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
-//    _mapView.delegate = self;
-//    
-//    [self.view addSubview:_mapView];
-    
-    
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
